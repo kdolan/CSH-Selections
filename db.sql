1ab55c547aa180c439e3be6363765ab694c64611
Kevin J Dolan <git@kevinjdolan.com>
2015-02-09 14:30:04 -0500

    Changes to db structure. Adds create user StoredProc.

--- db.sql
+++ db.sql
@@ -3,22 +3,38 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 08, 2015 at 11:05 PM
+-- Generation Time: Feb 09, 2015 at 02:27 PM
 -- Server version: 5.6.19-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `selections`
 --
+CREATE DATABASE IF NOT EXISTS `selections` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `selections`;
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spInsert_user`(IN `p_username` VARCHAR(25), IN `p_password` VARCHAR(32))
+BEGIN 
+    INSERT INTO user
+         (
+			   username,
+               password_md5
+         )
+    VALUES 
+         ( 
+           p_username,
+           p_password                    
+         ); 
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -30,7 +46,8 @@ CREATE TABLE IF NOT EXISTS `applicant` (
   `key` int(11) NOT NULL AUTO_INCREMENT,
   `applicant_id` varchar(25) NOT NULL,
   `application_placeholder` int(11) DEFAULT NULL,
-  PRIMARY KEY (`key`)
+  PRIMARY KEY (`key`),
+  UNIQUE KEY `applicant_id` (`applicant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -44,7 +61,9 @@ CREATE TABLE IF NOT EXISTS `score` (
   `applicant` varchar(25) NOT NULL,
   `reviewer` varchar(25) NOT NULL,
   `score_1` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `applicant` (`applicant`),
+  KEY `reviewer` (`reviewer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -56,10 +75,18 @@ CREATE TABLE IF NOT EXISTS `score` (
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
+  `password_md5` varchar(32) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `score`
+--
+ALTER TABLE `score`
+  ADD CONSTRAINT `fk_username` FOREIGN KEY (`reviewer`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `fk_applicant` FOREIGN KEY (`applicant`) REFERENCES `applicant` (`applicant_id`);
