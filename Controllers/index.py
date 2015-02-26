41b04cecc023f5a8e3b5ebc3ac6e6af4e474b41e
Kevin J Dolan <git@kevinjdolan.com>
2015-02-26 16:54:34 -0500

    Index page has working javascript to select applicants in a specific group

--- Controllers/index.py
+++ Controllers/index.py
@@ -8,12 +8,17 @@ def page_html(dbconn):
     group_options = _format_options(applicant_groups)
     print group_options
 
-    applicant_list = [] #List that hold a list of applicants for each group.
+    """The applicant_list holds a list of all applicants where the index of the
+    list is the group number. The size of the list is the largest group. The largest
+    group is the last item in the list because get_applicantGroups returns the
+    groups from smallest to largest. +1 for 0 indexed array."""
+    applicant_list = [''] * (applicant_groups[-1] + 1)
     for group in applicant_groups:
         applicants = dbconn.get_applicantInGroup(group)
-        applicant_list.append(applicants)
-    print(applicant_list)
-    return raw_html.format(group_options, "")
+        applicant_list[group] = applicants
+
+    js_applicant_array = _format_javascript_array(applicant_list)
+    return raw_html.format(js_applicant_array, group_options)
 
 def _format_options(optionList):
     returnString = ""
@@ -25,4 +30,8 @@ def _format_options(optionList):
 """Formats javascript array so that when the group drop down is updated
 the applicant dropdown can also be updated"""
 def _format_javascript_array(applicant_list):
-    pass
+    formated_applicant_list = []
+    for applicants_in_group in applicant_list:
+        formated_applicant_list.append(_format_options(applicants_in_group))
+
+    return str(formated_applicant_list)
