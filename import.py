bcf554d3766e6ba806a7e780b52a4b17787eb88a
Kevin J Dolan <git@kevinjdolan.com>
2015-04-06 22:31:36 -0400

    Pseudo for importing application ids into the db

--- import.py
+++ import.py
@@ -13,9 +13,41 @@ if __name__ == "__main__":
         for row in contents:
             matrix.append(row)
 
+    current_applicants = dbConn.get_allAplicants()
+    imported_applicants = [d[0] for d in matrix] #get the application id for all rows in file
+
+    new_applicants = list()
+    update_applicants = list()
+    notUpdated_applicants = list()
+
+    for applicant in imported_applicants:
+        if applicant in current_applicants: #If the applicant already exists in the db
+            update_applicants.append(applicant)
+        else
+            new_applicants.append(applicant)
+
+    for applicant in current_applicants:
+        if applicant not in imported_applicants:
+            notUpdated_applicants.append(applicant)
+
     for row in matrix:
         print(row)
-        #If applicant exists in db then update group
-        #If they do not exist then add them
-        #For all applicants not imported update the group to 0
+
+        application_id = row[0]
+        gender = row[1]
+
+        if(gender == "M"):
+            gender = 0
+        else
+            gender = 1
+
+        group = row[2]
+
+        if application_id in new_applicants:
+            #dbConn.insert_applicant
+        else:
+            #dbConn.update_applicant
+
+    for applicant in notUpdated_applicants:
+        #dbConn.update_clearGroup
 
